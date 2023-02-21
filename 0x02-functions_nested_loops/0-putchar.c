1b2c74bd61e6b4fd34bb9c49084975525dede31b
Samascogh <samsolomon363@gmail.com>
2023-02-21 08:15:11 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by samascogh
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p', 'u', 't', 'c', 'h', 'a', 'r'\'\n');
+return (0);
+}
