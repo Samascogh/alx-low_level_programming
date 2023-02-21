61c33da59d24d4b209674d2f69cf382b6e07e9ec
Samascogh <samsolomon363@gmail.com>
2023-02-21 08:29:15 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,17 @@
  * Return: 0
  */
 
+
 int main(void)
 {
 _putchar('_');
-_putchar('p', 'u', 't', 'c', 'h', 'a', 'r'\'\n');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
