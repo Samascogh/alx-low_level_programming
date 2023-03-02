a1614fa1b522218ed9ff311015ac1aec4146541d
Samascogh <samsolomon363@gmail.com>
2023-03-02 09:29:46 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,10 +14,11 @@ char *cap_string(char *str)
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
 	index++;
 	if (str[index - 1] == ' ' ||
-	str[index - 1] == ' \t' ||
-	str[index - 1] == ' \n' ||
+	str[index - 1] == '\t' ||
+	str[index - 1] == '\n' ||
 	str[index - 1] == ',' ||
 	str[index - 1] == ';' ||
+	str[index - 1] == '.' ||
 	str[index - 1] == '!' ||
 	str[index - 1] == '?' ||
 	str[index - 1] == '"' ||
