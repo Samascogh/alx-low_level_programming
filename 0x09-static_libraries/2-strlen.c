edd76e842823a0c14ec5042bac6f04bf196052ce
Samascogh <samsolomon363@gmail.com>
2023-03-10 08:32:55 -0800

    libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/**
+ *_strlen - function that returns the length of a string
+ * @s: check the lenght of the string
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	i++;
+
+	return (i);
+
+}
